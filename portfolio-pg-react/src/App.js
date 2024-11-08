4d2917bfd8ee5e83dc6908184ebf5a8e9023f882
Josue Castro <josuecaztro@gmail.com>
2024-11-07 22:44:14 -0500

    debugged route paths and pages

--- portfolio-pg-react/src/App.js
+++ portfolio-pg-react/src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './Containers/Home/homeindex';
 import AboutPage from './Containers/About/aboutindex';
@@ -9,6 +11,20 @@ import SkillsPage from './Containers/Skills/skillsindex';
 import './App.css';
 
 function App() {
+
+  //This is an attempt to fix the root pages issue. If no luck, delete this up to function invoke. 
+  /* UPDATE: Okay, it looks like for now it works.
+   The only true way to know for sure is to wait
+  until we code the navbar and we can toggle
+  between Routes. Thats how we will know for sure. */ 
+  const navigate = useNavigate();
+  function RedirectToHome(){
+    useEffect(() => {
+    navigate("/");
+    },[navigate]);
+  }
+  RedirectToHome();
+
   return (
     <div className="App">
 
